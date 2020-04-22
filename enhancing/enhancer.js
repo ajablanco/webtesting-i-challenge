fb1d58ec7f671e9c6b6e2b0f80fb8ee9234513ca
Aja Blanco <59661417+ajablanco@users.noreply.github.com>
2020-04-22 15:19:58 -0700

    MVP and first stretch goal

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,57 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
-}
+  if (item.enhancement < 20) {
+    const newItem = {
+      name: item.name,
+      enhancement: item.enhancement + 1,
+      durability: 100
+    }
+    return newItem ;
+  } else {
+    return item
+    }
+  }
+  
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15){
+    const newItem = {
+      name: item.name,
+      enhancement: item.enhancement,
+      durability: item.durability - 5
+  }
+  return newItem;
+} else if (item.enhancement > 16) {
+const newItem = {
+  name: item.name,
+  enhancement: item.enhancement - 1,
+  durability: item.durability - 10
+}
+return newItem;
+} else if (item.enhancement > 14) {
+  const newItem = {
+    name: item.name,
+    enhancement: item.enhancement,
+    durability: item.durability - 10
+} 
+return newItem;
+}
 }
 
 function repair(item) {
-  return { ...item };
+  const newItem = {
+    name: item.name,
+    enhancement: item.enhancement,
+    durability: 100
+  }
+  return newItem;
 }
 
+
 function get(item) {
-  return { ...item };
+  if (item.enhancement == 0) {
+   return item.name
+  } else if (item.enhancement > 0 ) {
+    return `[${item.enhancement}+] ${item.name}` }
 }
